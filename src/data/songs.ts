dbf6c62314d79906ba3ea4d7869899fc94c5e0a8
Behnoush Norouzali <behnoush.norouzali@recommerce.com>
2026-04-28 19:27:55 +0200

    vue supersimple app

---
+++ src/data/songs.ts
@@ -0,0 +1,23 @@
+export type Song = {
+  title: string;
+  youtubeId: string;
+  lyrics: string;
+};
+
+export const songs: Song[] = [
+  {
+    title: "Baby Shark",
+    youtubeId: "XqZsoesa55w",
+    lyrics: `Baby shark doo doo doo doo doo doo`
+  },
+  {
+    title: "Twinkle Twinkle Little Star",
+    youtubeId: "yCjJyiqpAuU",
+    lyrics: `Twinkle, twinkle, little star`
+  },
+  {
+    title: "If You're Happy",
+    youtubeId: "l4WNrvVjiTw",
+    lyrics: `If you're happy and you know it clap your hands!`
+  }
+];
